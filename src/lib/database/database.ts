9907488696298c993e583f963e77e6cea67c0c58
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-12 01:33:51 +0100

    Refactoring

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -5,12 +5,12 @@ import { development, production } from '../../../knexfile';
 const environment = mode || 'development';
 let config: Knex.Config;
 switch (environment) {
-	case 'development':
-		config = development;
-	case 'production':
-		config = production;
-	default:
-		config = development;
+  case 'development':
+    config = development;
+  case 'production':
+    config = production;
+  default:
+    config = development;
 }
 
 export default knex(config);
