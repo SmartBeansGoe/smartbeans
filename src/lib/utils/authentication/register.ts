c15fc4547a1eb0609bfd9649ef6cbf83a312fac9
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-12 01:55:30 +0100

    Add register by username & password and by username

---
+++ src/lib/utils/authentication/register.ts
@@ -0,0 +1,51 @@
+import database from '$lib/database/database';
+import { passwordHash } from './password';
+
+export const createUser = async (
+  username: string,
+  password?: string,
+  displayName?: string
+): Promise<boolean> => {
+  if (password) {
+    return createUserWithPassword(username, password, displayName);
+  } else {
+    return createUserWithoutPassword(username, displayName);
+  }
+};
+
+const createUserWithoutPassword = async (
+  username: string,
+  displayName?: string
+): Promise<boolean> => {
+  try {
+    await database.table('users').insert({
+      username: username,
+      displayName: displayName,
+      ltiEnabled: true,
+      avatar: { hat: null, pants: null, shirt: null, skin: '#012934' }
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const createUserWithPassword = async (
+  username: string,
+  password: string,
+  displayName?: string
+): Promise<boolean> => {
+  const hash = await passwordHash(password);
+  try {
+    await database.table('users').insert({
+      username: username,
+      password: hash,
+      ltiEnabled: false,
+      displayName: displayName,
+      avatar: { hat: null, pants: null, shirt: null, skin: '#012934' }
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
