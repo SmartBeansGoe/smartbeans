1b847f66ec69aa7a96522b0c7ec515d7f3a6f90c
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-12 01:42:28 +0100

    Add config

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export default {
+  sessionDuration: 43200,
+  staticAchievementPath: '/img/achievements',
+  staticAchievementFilePath: '/img/achievements/achievements.json',
+  staticAssetPath: '/img/assets',
+  staticAssetFilePath: '/img/assets/assets.json',
+  padUrl: 'change-me',
+  sandboxUrl: 'change-me',
+  achievementUrl: 'change-me',
+  registrationKeys: ['change-me'],
+  apiKeys: ['change-me'],
+  TOKEN_LENGTH: 36,
+  ltiConsumerSecret: 'change-me',
+  leaderboardUsersLastSubmissionTimeout: 1000 * 60 * 60 * 24 * 30
+};
