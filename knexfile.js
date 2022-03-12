173eb6e81f995101d09cef5902e6922314dd864e
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-12 01:27:42 +0100

    Change client to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,37 @@
 // Update with your config settings.
 
 export const development = {
-	client: 'mysql',
-	connection: {
-		database: 'smartbeans2',
-		user: 'smartbeans',
-		password: 'smartbeans'
-	},
-	pool: {
-		min: 2,
-		max: 30
-	},
-	migrations: {
-		tableName: 'knex_migrations'
-	}
+  client: 'postgres',
+  connection: {
+    host: "change-me",
+    port: 5432,
+    database: 'smartbeans',
+    user: 'smartbeans',
+    password: 'smartbeans'
+  },
+  pool: {
+    min: 2,
+    max: 30
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
 
 export const production = {
-	client: 'mysql',
-	connection: {
-		database: 'smartbeans',
-		user: 'smartbeans',
-		password: 'smartbeans'
-	},
-	pool: {
-		min: 2,
-		max: 30
-	},
-	migrations: {
-		tableName: 'knex_migrations'
-	}
+  client: 'postgres',
+  connection: {
+    host: "change-me",
+    port: 5432,
+    database: 'smartbeans',
+    user: 'smartbeans',
+    password: 'smartbeans'
+  },
+  pool: {
+    min: 2,
+    max: 30
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
