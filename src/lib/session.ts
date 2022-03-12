483417179e3e3550abeb7c865912796caa667b45
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-12 01:45:04 +0100

    Add InvalidError

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -5,3 +5,9 @@ export const generateToken = (length: number) => {
   return result;
 };
 
+export class InvalidError extends Error {
+  constructor(msg: string) {
+    super(msg);
+    Object.setPrototypeOf(this, InvalidError.prototype);
+  }
+}
