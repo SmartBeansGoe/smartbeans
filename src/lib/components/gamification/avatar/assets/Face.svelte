b77dc62e752d392af72aee95c4d03fedf8590a52
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:38:11 +0100

    Add face component

---
+++ src/lib/components/gamification/avatar/assets/Face.svelte
@@ -0,0 +1,41 @@
+<g
+  ><g transform="translate(-38.136 -44.878)"
+    ><g transform="translate(-3.1432 3.5282)"
+      ><path
+        d="m71.048 79.603s7.7237 8.7063 17.27 0.26978"
+        fill="none"
+        stroke="#000"
+        stroke-width="1.165"
+      /><g stroke-linecap="round" stroke-linejoin="bevel"
+        ><g
+          ><ellipse
+            cx="73.014"
+            cy="69.163"
+            rx="2.6594"
+            ry="4.6438"
+            fill="#fffcec"
+            stroke="#fffcec"
+            stroke-width="1.4589"
+          /><circle cx="73.014" cy="71.147" r="2.8063" stroke="#000" stroke-width="1.165" /></g
+        ><g
+          ><ellipse
+            cx="87.131"
+            cy="68.854"
+            rx="2.6594"
+            ry="4.6438"
+            fill="#fffcec"
+            stroke="#fffcec"
+            stroke-width="1.4589"
+          /><circle
+            cx="87.139"
+            cy="70.593"
+            r="3.0068"
+            fill="#0a0003"
+            stroke="#000"
+            stroke-width="1.165"
+          /></g
+        ></g
+      ></g
+    ></g
+  ></g
+>
