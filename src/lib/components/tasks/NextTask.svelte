08369798d20f17f282edd3b5d353c65e2d880f27
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:49:38 +0100

    Add next task component

---
+++ src/lib/components/tasks/NextTask.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { CourseTask } from '$lib/utils/types/task';
+  import Tag from '../utils/Tag.svelte';
+
+  export let task: CourseTask;
+</script>
+
+<section class="flex flex-col h-full">
+  <div class="grow">{task.taskDescription.title}</div>
+
+  <div class="flex justify-self-end gap-2">
+    {#each task.tags as tag}
+      <Tag {tag} showPoints={true} />
+    {/each}
+  </div>
+</section>
