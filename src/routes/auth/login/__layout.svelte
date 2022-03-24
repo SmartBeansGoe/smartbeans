08d59fc15013a57de36c51352504763006d7bf6d
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 20:35:31 +0100

    Add login layout

---
+++ src/routes/auth/login/__layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = async ({ session }) => {
+    if (session.user !== null) return { status: 302, redirect: `/` };
+    return {};
+  };
+</script>
+
+<div class="hero-content flex-col">
+  <section class="text-center">
+    <h1 class="text-5xl font-bold">SmartBeans</h1>
+    <p class="py-6">Die Platform für das Lernen von Programmiersprachen...</p>
+  </section>
+  <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100  bg-opacity-90">
+    <div class="card-body">
+      <slot />
+    </div>
+  </div>
+</div>
