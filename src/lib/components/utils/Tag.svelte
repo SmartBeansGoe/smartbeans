19fea7127ba8326e0a010cc7bc59dda57fd81985
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:44:57 +0100

    Add tag component

---
+++ src/lib/components/utils/Tag.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Tag } from '$lib/utils/types/tag';
+
+  export let tag: Tag;
+  export let showPoints: boolean = false;
+</script>
+
+<span class="tooltip-info" class:tooltip={tag.points !== undefined} data-tip="{tag.points} Punkte">
+  <button class="btn btn-xs rounded-full btn-info" on:click>
+    {tag.name}{#if showPoints}{tag.points ? `: ${tag.points}` : ''}{/if}
+  </button>
+</span>
