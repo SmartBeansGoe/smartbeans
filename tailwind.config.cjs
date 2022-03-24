e2fdd2e29aee789e31df2b807987ebe14c4a0001
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 20:36:11 +0100

    Add tailwind additional width classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@ const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      width: {
+        "1/2-gap-4": "calc(50% - 0.5rem)",
+        "1/3-gap-4": "calc(33% - 0.5rem)",
+        "1/4-gap-4": "calc(25% - 0.5rem)",
+      }
+    }
   },
 
   plugins: [require("daisyui")],
