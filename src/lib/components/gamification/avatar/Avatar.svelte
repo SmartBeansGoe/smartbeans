b5f03fd1144dfd67dd5d5bac7a0d5414446eaf6c
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:41:01 +0100

    Add avatar component

---
+++ src/lib/components/gamification/avatar/Avatar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Avatar } from '$lib/utils/types/avatar';
+
+  import Asset from './assets/Asset.svelte';
+  import Body from './assets/Body.svelte';
+  import Face from './assets/Face.svelte';
+  export let avatar: Avatar;
+  let bodyColor = avatar.skin ? avatar.skin : '#558844';
+</script>
+
+{#if avatar}
+  <Body color={bodyColor}>
+    <Face slot="face" />
+    <Asset slot="pants" id={avatar.pants} />
+    <Asset slot="shirt" id={avatar.shirt} />
+    <Asset slot="hat" id={avatar.hat} />
+  </Body>
+{/if}
