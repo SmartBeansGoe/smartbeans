d0abae8af0937f5f25636ae524f384e1516c84d3
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 20:35:22 +0100

    Add auth layout reset

---
+++ src/routes/auth/__layout.reset.svelte
@@ -0,0 +1,14 @@
+<div class="hero h-screen bg-base-200">
+  <div class="h-full w-full absolute p-20 opacity-50">
+    <div class="h-full hero backdrop" />
+  </div>
+  <slot />
+</div>
+
+<style>
+  .backdrop {
+    background-image: url('/smartbeans-logo.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+</style>
