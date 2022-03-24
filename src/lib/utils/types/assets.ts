c005f078f12a684e7bb0db8dff58a6de8d2d1332
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:39:06 +0100

    Add asset types and schemas

--- src/lib/utils/types/assets.ts
+++ src/lib/utils/types/assets.ts
@@ -1,8 +1,20 @@
-export type Asset = {
-  id: string;
-  path: string;
-  attributes: Array<string>;
-  name: string;
-  outfitId: string;
-  type: string;
-};
+import * as yup from 'yup';
+
+export const schemaAssetUnlocked = yup.object().shape({
+  assetId: yup.string().required(),
+  user: yup.string().required()
+});
+export type AssetUnlocked = yup.InferType<typeof schemaAssetUnlocked>;
+export type AssetUnlockedRow = AssetUnlocked;
+
+export const schemaAsset = yup.object().shape({
+  id: yup.string().required(),
+  outfitId: yup.string().required().max(255),
+  type: yup.string().oneOf(['hat', 'pants', 'shirt']).max(255),
+  name: yup.string().required().max(255),
+  details: yup.object().default({}).required()
+});
+
+export type Asset = yup.InferType<typeof schemaAsset>;
+
+export type AssetRow = Asset;
