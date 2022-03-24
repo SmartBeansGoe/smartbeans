f514898e989d1ed66072cd1a5bf349e3cd50e5f1
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:37:42 +0100

    Add asset notification component

---
+++ src/lib/components/gamification/avatar/assets/AssetNotification.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Asset from './Asset.svelte';
+
+  export let id: string;
+  export let text: string;
+</script>
+
+<div>
+  {text}
+  <div class="w-3/12">
+    <Asset {id} />
+  </div>
+</div>
