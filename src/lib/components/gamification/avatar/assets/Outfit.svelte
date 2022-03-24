86ab0129ea563b88e5bb16c7df42dc21344b12cf
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:41:24 +0100

    Add outfit component

---
+++ src/lib/components/gamification/avatar/assets/Outfit.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Asset from './Asset.svelte';
+  import Body from './Body.svelte';
+
+  export let pantsId: string;
+  export let hatId: string;
+  export let shirtId: string;
+  export let skin: string;
+  export let alpha: number = 1.0;
+</script>
+
+<Body color={skin} {alpha}>
+  <slot />
+  <Asset slot="pants" id={pantsId} />
+  <Asset slot="shirt" id={shirtId} />
+  <Asset slot="hat" id={hatId} />
+</Body>
