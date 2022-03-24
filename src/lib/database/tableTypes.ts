bbfd53e7dddf4f9748d7715b4e7b128457db08bc
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:51:49 +0100

    Add asset row types

--- src/lib/database/tableTypes.ts
+++ src/lib/database/tableTypes.ts
@@ -3,6 +3,7 @@ import type {
   AchievementUnlockedRow,
   CourseAchievementRow
 } from '$lib/utils/types/achievement';
+import type { AssetRow, AssetUnlockedRow } from '$lib/utils/types/assets';
 import type { CourseMappingRow, CourseRow } from '$lib/utils/types/course';
 import type { SessionRow } from '$lib/utils/types/session';
 import type { SubmissionRow } from '$lib/utils/types/submission';
@@ -12,7 +13,9 @@ import type { UserRow } from '$lib/utils/types/user';
 declare module 'knex/types/tables' {
   interface Tables {
     achievements: AchievementRow;
-    achievementUnlocked: AchievementUnlockedRow;
+    unlockedAchievements: AchievementUnlockedRow;
+    assets: AssetRow;
+    unlockedAssets: AssetUnlockedRow;
     courses: CourseRow;
     courseAchievement: CourseAchievementRow;
     courseMapping: CourseMappingRow;
