d41c8b2902acf8a4470d3b1e8de150338fb669b1
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 19:50:29 +0100

    Add next task notification

---
+++ src/lib/components/tasks/NextTaskNotification.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { CourseTask } from '$lib/utils/types/task';
+
+  import Icon from '@iconify/svelte';
+  import { toast } from '@zerodevx/svelte-toast';
+
+  export let task: CourseTask;
+
+  function handleClick() {
+    toast.pop();
+  }
+</script>
+
+<a href="/tasks/{task.taskid}" on:click={handleClick}>
+  <div class="flex">
+    <span class="flex items-center"><Icon icon="mdi:chevron-right" width={36} height={36} /></span>
+    <article>
+      <h3>Nächste Aufgabe:</h3>
+      {task.taskDescription.shortname}
+    </article>
+  </div>
+</a>
