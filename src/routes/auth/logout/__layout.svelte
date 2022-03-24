07d244a8b4998dac30720ae5b9a7ab34e1a16350
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 20:40:13 +0100

    Add logout layout

---
+++ src/routes/auth/logout/__layout.svelte
@@ -0,0 +1,7 @@
+<div class="hero-content flex-col">
+  <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 bg-opacity-90">
+    <div class="card-body">
+      <slot />
+    </div>
+  </div>
+</div>
