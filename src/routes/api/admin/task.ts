5229c471e20eaa58c83f15a201105d1974e0c4b7
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-24 20:23:12 +0100

    Add task route documentation and add unlockable assets

--- src/routes/api/admin/task.ts
+++ src/routes/api/admin/task.ts
@@ -3,13 +3,60 @@ import { schemaTaskPost, type TaskPost } from '$lib/utils/types/task';
 import type { RequestHandler } from '@sveltejs/kit';
 import * as yup from 'yup';
 
+const asyncFilter = async (arr, predicate) => {
+  const results = await Promise.all(arr.map(predicate));
+  return arr.filter((_v, index) => results[index]);
+};
+
+/**
+ * ### GET `/api/admin/task`
+ *
+ * Add new tasks with course task configurations.
+ *
+ * ***Input***
+ * ```
+ * {
+ *     taskid: number,
+ *     taskDescription: {
+ *         defaultEditorInput: string,
+ *         shortname: string,
+ *         task: string,
+ *         title: string,
+ *     },
+ *     lang: "python" | "c",
+ *     solution: string,
+ *     tests: string[],
+ *     courseMetaData: [
+ *       course: string,
+ *       tags: [{
+ *         name: string,
+ *         points?: number,
+ *       }],
+ *       orderBy: number,
+ *       prerequisites: [],
+ *       unlockableAssets: [],
+ *    ]
+ * }
+ * ```
+ *
+ * ***Output***
+ * ```
+ * {
+ *   deleted: [] // unlockable assets which are not available
+ * }
+ * ```
+ *
+ * ***Errors***
+ * - 400: Body Validation Error
+ * - 401: Unauthorized
+ */
 export const post: RequestHandler = async ({ request }) => {
   const body = await request.json();
   let task: TaskPost;
 
   // Validate body data
   try {
-    task = await schemaTaskPost.validate(body);
+    task = await schemaTaskPost.validate(body, { stripUnknown: true });
   } catch (error) {
     if (error instanceof yup.ValidationError) {
       return { status: 400, body: JSON.stringify(error.errors) };
@@ -38,7 +85,19 @@ export const post: RequestHandler = async ({ request }) => {
       .where({ taskid: task.taskid });
   }
 
+  let deletedUnlockableAssets = [];
+
   task.courseMetaData.forEach(async (courseMetaData) => {
+    // Filter unlockable assets availability
+    let assets: any;
+    if (courseMetaData.unlockableAssets) {
+      assets = await asyncFilter(courseMetaData.unlockableAssets, async (assetId) => {
+        let asset = await database.table('assets').select('*').where('id', assetId).first();
+        if (asset === undefined) deletedUnlockableAssets.push(assetId);
+        return asset !== undefined;
+      });
+    }
+
     try {
       // Insert courseTask into database
       await database.table('courseTask').insert({
@@ -46,7 +105,8 @@ export const post: RequestHandler = async ({ request }) => {
         course: courseMetaData.course,
         orderBy: courseMetaData.orderBy,
         prerequisites: JSON.stringify(courseMetaData.prerequisites), // knex needs a stringified json
-        tags: JSON.stringify(courseMetaData.tags) // knex needs a stringified json
+        tags: JSON.stringify(courseMetaData.tags), // knex needs a stringified json
+        unlockableAssets: JSON.stringify(assets) // knex needs a stringified json
       });
     } catch {
       // Update courseTask into database
@@ -56,12 +116,13 @@ export const post: RequestHandler = async ({ request }) => {
           .update({
             orderBy: courseMetaData.orderBy,
             prerequisites: JSON.stringify(courseMetaData.prerequisites), // knex needs a stringified json
-            tags: JSON.stringify(courseMetaData.tags) // knex needs a stringified json
+            tags: JSON.stringify(courseMetaData.tags), // knex needs a stringified json
+            unlockableAssets: JSON.stringify(assets) // knex needs a stringified json
           })
           .where({ taskid: task.taskid, course: courseMetaData.course });
       });
     }
   });
 
-  return {};
+  return { body: { deleted: { unlockableAssets: deletedUnlockableAssets } } };
 };
